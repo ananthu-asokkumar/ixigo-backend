9ddcf5579409582be30f5a7f95f7d2b542da90f8
ananthu-asokkumar <ananthu_fw10_048@masai.school>
2021-10-27 17:44:54 +0530

    first

---
+++ src/server.js
@@ -0,0 +1,26 @@
+
+
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+require("dotenv").config();
+
+const connect = require("./configs/db");
+
+
+const flightController = require("./controllers/flight.controllers");
+const signupController=require("./controllers/signup.controllers")
+
+
+app.use("/flights", flightController);
+app.use("./signup", signupController);
+// app.set("views", path.join(__dirname, "views/"));
+
+
+app.listen(process.env.PORT||5000, async () => {
+  await connect();
+  console.log("listening to port 5000");
+});
